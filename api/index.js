82e054e1dfa9d56162735ae70699308798c9a18b
Hassan Khaled <58123330+HassanKha@users.noreply.github.com>
2024-01-01 18:48:24 +0200

    Update index.js to deply

--- api/index.js
+++ api/index.js
@@ -12,13 +12,17 @@ import { MessageModel } from "./models/Message.js";
 import fs from "fs";
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import path from 'path'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 dotenv.config();
+const _dirname = path.resolve();
+
 const bcryptSalt = bcrypt.genSaltSync(10);
 const app = express();
+app.use(express.static(path.join(_dirname,'/my-project/dist')));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(
